1ebaa875863f06b6bab65e8cb9d5e381589541ed
Pedro Menezes <57593251+pjmenezes@users.noreply.github.com>
2023-01-08 17:40:21 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "ip" {
-  value = google_compute_instance.vm_instance.network_interface.0.network_ip
-}
+    value = "google_compute_instance.name_vm_instance.network_interface.0.network_ip"
+} 
